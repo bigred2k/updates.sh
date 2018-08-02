d8ac536f367a370d5be66e3500483c1814a0b44b
username <jimmy.woodard@comcast.net>
2018-08-02 00:02:34 -0600

    added mail functionality

--- updates.sh
+++ updates.sh
@@ -5,9 +5,11 @@ uptime="$(uptime)"
 freemem="$(free -m)"
 freedisk="$(df -h | grep '/'|sort -nr -k5)"
 docroots="$(cat /etc/httpd/conf.d/*.conf |grep DocumentRoot | grep -v '#'|awk '{print $2}'|sort |uniq)"
-#docroots2="$(sudo /usr/sbin/httpd -S | /bin/egrep "(namevhost|:80)" |awk '{print $4}' )"
-#docroots3="$(/bin/grep -v "#" $docroots  | /bin/grep -io "DocumentRoot.*" | /bin/cut -d " " -f2 | /usr/bin/uniq)"
+hostname=$(hostname)
 
+
+mkdir -p /opt/scripts/
+rm -rf /opt/scripts/updates.txt
 echo "Uptime:"
 echo "$uptime"
 echo
@@ -21,8 +23,11 @@ echo "Docroots found in /etc/httpd/conf.d/*.conf:"
 echo "$docroots"
 echo
 echo
-for docroot in $docroots; do echo; cd $docroot ; echo $(pwd) ;wp core version  --allow-root 2>/dev/null; wp plugin list --allow-root 2>/dev/null | grep -i 'available' ; wp theme list --allow-root 2>/dev/null | grep -i 'available' ; drush status | grep -i 'Drupal version';drush up --security-only -n | grep -i 'secur'; done
+
+for docroot in $docroots; do echo; cd $docroot ; echo $(pwd) ;wp core version  --allow-root 2>/dev/null; wp plugin list --allow-root 2>/dev/null | grep -i 'available' ; wp theme list --allow-root 2>/dev/null | grep -i 'available' ; drush status 2>/dev/null | grep -i 'Drupal version';drush up --security-only -n 2>/dev/null | grep -i 'SECURITY UPDATE available' ;done >> /opt/scripts/updates.txt
 
 finish_time="$(date +%s)"
+
+mail -s 'CMS updates for $hostname' jwoodard@contegix.com < /opt/scripts/updates.txt
 
 echo "Time duration: $((finish_time - start_time)) secs."
